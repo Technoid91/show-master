0484c7cbd39b05cfd061dd3a53b216e9b421be96
Technoid <Technoid@Sazonov.local>
2023-08-23 16:57:24 +0100

    Add function to ask questions and switch to the next one

--- assets/js/script.js
+++ assets/js/script.js
@@ -7,21 +7,34 @@ for (show of showPicker){
     });
 }
 
-
 /*
 * Function to pick the question pool based on user's choice
 */
 function runGame(pickedShow){
     if (pickedShow === 'himym'){
-        runQuiz(himymQuiz);
+        currentQuiz = himymQuiz;
     } else if (pickedShow === 'tbbt'){
-        runQuiz(tbbtQuiz);
+        currentQuiz = tbbtQuiz;
     } else if (pickedShow === 'friends'){
-        runQuiz(friendsQuiz);
+        currentQuiz = friendsQuiz;
     } else {
         console.log(`Wrong show! ${pickedShow} does not exist`);
         throw(`Wrong show! ${pickedShow} does not exist`);
     }
+
+    /* Hide show picker field with show posters and show quiz field */
+    let pickerField = document.getElementsByClassName('show-picker')[0];
+    let quizField = document.getElementsByClassName('quiz-field')[0];
+    pickerField.setAttribute('hidden', 'true');
+    quizField.removeAttribute('hidden');
+
+    /* Show first question */
+    askQuestion(currentQuiz[0]);
+    let nextQuestion = document.getElementsByTagName('button')[0];
+    nextQuestion.addEventListener('click', function(){
+       questionNumber = Number(document.getElementsByClassName('question-number')[0].textContent);
+       askQuestion(currentQuiz[questionNumber]);
+    })
 }
 
 /*
@@ -44,22 +57,14 @@ function quizTitle(show){
     document.getElementsByTagName('h2')[0].textContent = quizTitle;
 }
 
-function runQuiz(currentQuiz){
-
-    let pickerField = document.getElementsByClassName('show-picker')[0];
-    let quizField = document.getElementsByClassName('quiz-field')[0];
-    pickerField.setAttribute('hidden', 'true');
-    quizField.removeAttribute('hidden');
-
-    askQuestion(currentQuiz[0]);
-    checkAnswer(currentQuiz[0]);
-
-
-}
-
 function askQuestion(questionData){
     let question = questionData[0];
+    let correctAnswer = questionData[1];
     let answers = [questionData[1], questionData[2], questionData[3], questionData[4]];
+
+/*    console.log('QUESTION: '+question);
+    console.log('ANSWERS: ' + answers);
+    console.log('CORRECT: '+ correctAnswer);*/
 
     let img = document.createElement('img');
     img.src = 'assets/images/test-img.png';
@@ -67,40 +72,48 @@ function askQuestion(questionData){
     let questImage = document.getElementsByClassName('quiz-image')[0];
     questImage.innerHTML = '';
     questImage.appendChild(img);
+
     document.getElementsByClassName('question')[0].textContent = question;
     questionAnswers = document.getElementsByClassName('answer');
 
     for (let i = 0; i < answers.length; i++){
         questionAnswers[i].textContent = answers[i];
+
         questionAnswers[i].addEventListener('click', function(event){
             let oldChoice = document.getElementsByClassName('user-answer');
-            for (each of oldChoice){
-                each.classList.remove('user-answer');
-            }
+            if (oldChoice){
+                for (each of oldChoice){
+                    each.classList.remove('user-answer');
+                }
             questionAnswers[i].classList.add('user-answer');
+            }
         })
     }
-    let quizField = document.getElementsByClassName('quiz-field')[0];
+    if (document.getElementsByClassName('user-answer')[0]){
+        let submit = document.getElementsByTagName('button')[0];
+        submit.addEventListener('click', checkAnswer(correctAnswer));
+    }
+    /*let quizField = document.getElementsByClassName('quiz-field')[0];*/
+
 }
 
-function checkAnswer(questionData){
+function checkAnswer(correctAnswer){
+    console.log("ANSWER TO CHECK: "+correctAnswer);
 
-    let submButton = document.getElementsByTagName('button')[0];
-    submButton.addEventListener('click', function(){
-        let correctAnswer = questionData[1];
-        let userAnswer = document.getElementsByClassName('user-answer')[0].textContent;
-        console.log(`User picked ${userAnswer}`);
-        console.log(`Correct answer: ${correctAnswer}`);
-        if (userAnswer === correctAnswer){
-            alert('Correct!')
-        }
-        else{
-            alert('You are wrong :(   The correct answer is '+correctAnswer);
-        }
-    })
-
-
-
+    let answerElement = document.getElementsByClassName('user-answer')[0];
+    let userAnswer = answerElement.textContent;
+    console.log(`User picked ${userAnswer}`);
+    console.log(`Correct answer: ${correctAnswer}`);
+    if (userAnswer === correctAnswer){
+        alert('Correct!')
+    }
+    else{
+       alert('You are wrong :(   The correct answer is '+correctAnswer);
+    }
+    answerElement.classList.remove('user-answer');
+    let nextQuestion = Number(document.getElementsByClassName('question-number')[0].textContent);
+    nextQuestion++;
+    document.getElementsByClassName('question-number')[0].textContent = nextQuestion;
 }
 
 function randomizeElements(initialArray){
@@ -118,7 +131,8 @@ let himymQuiz = [
     ["How called the Marshal's and Barney's annual tradition?",
     "Slapsgiving Day",
     "St Barney's Day",
-    "Desperate "],
+    "Desperate ",
+    "Last Answer"],
     ["Each Halloween Ted was looking for a mystery girl dressed as...",
     "Slutty pumpkin",
     "Space spud",
